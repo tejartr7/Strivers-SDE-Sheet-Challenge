e6fce5ecdaff25038d5de467357bedbc4ca3894d
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-06-25 14:12:26 +0530

    Update Stack using queue.java

--- Stack using queue.java
+++ Stack using queue.java
@@ -1,54 +1,77 @@
-import java.util.*;
-import java.io.*;
+import java.util.* ;
+
+import java.io.*; 
 
 public class Stack {
 
-    // Define the data members.
-    Queue<Integer> a = new LinkedList<>();
-    Queue<Integer> b = new LinkedList<>();
-    int top;
+    private Deque<Integer> queue1;    
+
+ 
+
     public Stack() {
-        // Implement the Constructor.
+
+        queue1 = new ArrayDeque<Integer>();        
+
     }
 
-    /*----------------- Public Functions of Stack -----------------*/
+ 
 
     public int getSize() {
-        return a.size();
-        // Implement the getSize() function.
+
+        return queue1.size();
+
     }
+
+ 
 
     public boolean isEmpty() {
-        // Implement the isEmpty() function.
-        return a.isEmpty() && b.isEmpty();
+
+        if(queue1.isEmpty()){
+
+            return true;
+
+        }
+
+        return false;
+
     }
 
-    public void push(int x) {
-        // Implement the push(element) function.
-        a.add(x);
-        top=x;
+ 
+
+    public void push(int element) {
+
+        queue1.push(element);
+
     }
+
+ 
 
     public int pop() {
-        // Implement the pop() function.
-        //int temp = 0;
-        if (a.isEmpty() && b.isEmpty())
+
+        if(isEmpty()){
+
             return -1;
-        while(a.size()>1)
-        {
-          top=a.remove();
-          b.add(top);  
+
         }
-        int x=a.peek();
-        a.remove();
-        Queue<Integer> temp=a;
-        a=b;
-        b=temp;
-        return x;
+
+        return queue1.poll();
+
     }
 
+ 
+
     public int top() {
-        // Implement the top() function.
-        return top;
+
+         if(isEmpty()){
+
+            return -1;
+
+        }
+
+ 
+
+        return queue1.getFirst();
+
     }
+
 }
