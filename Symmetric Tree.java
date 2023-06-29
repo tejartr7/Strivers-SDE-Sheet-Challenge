abbccb6f423de1882e45a1c8c29855801d0667c1
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-06-29 12:06:13 +0530

    Update Symmetric Tree.java

--- Symmetric Tree.java
+++ Symmetric Tree.java
@@ -1,28 +1,11 @@
-/*****************************************************************
- * 
- * Following is the representation for the Binary Tree Node:
- * 
- * class BinaryTreeNode<T> {
- * T data;
- * BinaryTreeNode<T> left;
- * BinaryTreeNode<T> right;
- * 
- * public BinaryTreeNode(T data) {
- * this.data = data;
- * }
- * }
- * 
- ****************************************************************/
-
+import java.io.*;
 public class Solution {
     public static boolean helper(BinaryTreeNode<Integer> a, BinaryTreeNode<Integer> b) {
-        if (a == null)
-            return b == null;
-        if (b == null)
-            return a == null;
-        if (a.data != b.data)
+        if (a == null && b==null)
+            return true;
+        if (a==null || b==null || a.data != b.data)
             return false;
-        return helper(a.left, b.right) && helper(a.right, b.left);
+        return helper(a.left, b.right);
     }
 
     public static boolean isSymmetric(BinaryTreeNode<Integer> root) {
