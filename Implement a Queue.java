0dda8f906aac3f49e708d2c82b1b5a5ccc14bfc0
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-07-06 23:12:20 +0530

    Create Implement a Queue.java

---
+++ Implement a Queue.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Queue {
+    private int[] data;
+    int end = 0;
+    Queue() {
+        this.data = new int[5000];
+    }
+
+    /*----------------- Public Functions of Queue -----------------*/
+    boolean isEmpty() {
+        return end == 0;
+    }
+    void enqueue(int x) {
+        data[end++] = x;
+    }
+    int dequeue() {
+        if (isEmpty()) {
+            return -1;
+        }
+        int removed = data[0];
+        for (int i = 1; i < end; i++) {
+            data[i - 1] = data[i];
+        }
+        end--;
+        return removed;
+    }
+    int front() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return data[0];
+
+    }
+
+}
