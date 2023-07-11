5bd9fd9943b722686c5d9a63e29b4b29e39b81f3
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-07-11 12:46:18 +0530

    Create Set Matrix Zeros.java

---
+++ Set Matrix Zeros.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    private static void make(int aa[][], int x, int y) {
+        int l = aa.length;
+        int b = aa[0].length;
+        int i;
+        for (i = 0; i < l; i++) {
+            aa[i][y] = 0;
+        }
+        for (i = 0; i < b; i++) {
+            aa[x][i] = 0;
+        }
+
+    }
+
+    public static void setZero(int[][] matrix) {
+        int l = matrix.length;
+        int b = matrix[0].length;
+        int i, j;
+        int a[][] = new int[l][b];
+        for (i = 0; i < l; i++) {
+            for (j = 0; j < b; j++) {
+                a[i][j] = matrix[i][j];
+            }
+        }
+        for (i = 0; i < l; i++) {
+            for (j = 0; j < b; j++) {
+                if (a[i][j] == 0) {
+                    make(matrix, i, j);
+
+                }
+            }
+        }
+    }
+
+    public static void setZeros(int matrix[][]) {
+        // Write your code here..
+        setZero(matrix);
+    }
+
+}
