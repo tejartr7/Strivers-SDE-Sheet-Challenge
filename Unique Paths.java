20912c95c6ef18ed434cacdc1b036fc618192889
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-07-16 23:11:46 +0530

    Create Unique Paths.java

---
+++ Unique Paths.java
@@ -0,0 +1,30 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static int uniquePaths(int m, int n) {
+		// Write your code here.
+		int dp[][]=new int[m][n];
+        int i,j;
+        //top down method of tabulation
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(i==j && i==0)
+                {
+                    dp[i][j]=1;
+                }
+                else
+                {
+                    int a=0,b=0;
+                    if(i>0)
+                        a+=dp[i-1][j];
+                    if(j>0)
+                        b+=dp[i][j-1];
+                    dp[i][j]=a+b;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+	}
+}
